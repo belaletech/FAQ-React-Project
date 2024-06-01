c5bb694f31deadc69952bdb3fa01344bf6ba396f
belaletech <belalahmadswnbhr@gmail.com>
2024-06-01 19:19:24 +0530

    FAQ React appp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Accordion from "./component/accordian";
+
+
+const App =()=>{
+    return (
+        <div className="App">
+        <Accordion/>
+        </div>
+    );
+}
+
+export default App;
